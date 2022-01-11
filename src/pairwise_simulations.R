2083448d2ee195fbc154a87c65f9f43c6fca9c32
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-11 12:47:11 -0800

    👩🏿‍💻 Read biological parameters

--- src/pairwise_simulations.R
+++ src/pairwise_simulations.R
@@ -2,3 +2,4 @@ source("src/rea.R")
 
 tagCent <- read.table("data/raw/GMIStaticDeviceLocations.csv",sep=",",header=T)
 tmpshape <- rgdal::readOGR(dsn = "data/raw", layer = "GreatMercPerimiter")
+parameters <- read.table("data/raw/GMIParameters.csv",sep=",",header=T)
