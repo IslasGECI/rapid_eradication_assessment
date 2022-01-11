e9a901e31c88a014f72525ff1840983e2804cd8b
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-11 12:50:54 -0800

    👩🏿‍💻 Read session data

--- src/pairwise_simulations.R
+++ src/pairwise_simulations.R
@@ -3,3 +3,5 @@ source("src/rea.R")
 tagCent <- read.table("data/raw/GMIStaticDeviceLocations.csv",sep=",",header=T)
 tmpshape <- rgdal::readOGR(dsn = "data/raw", layer = "GreatMercPerimiter")
 parameters <- read.table("data/raw/GMIParameters.csv",sep=",",header=T)
+sessiondat <- read.table("data/raw/GMIMonitoringData.csv",sep=",",header=T)
+
