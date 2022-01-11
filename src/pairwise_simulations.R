a97f7997a7611bb4c3407652ad58dd09d3b1b02e
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-11 12:53:35 -0800

    🔧 Set up options of spatstat

--- src/pairwise_simulations.R
+++ src/pairwise_simulations.R
@@ -1,7 +1,10 @@
 source("src/rea.R")
+library('spatstat')
 
 tagCent <- read.table("data/raw/GMIStaticDeviceLocations.csv",sep=",",header=T)
 tmpshape <- rgdal::readOGR(dsn = "data/raw", layer = "GreatMercPerimiter")
 parameters <- read.table("data/raw/GMIParameters.csv",sep=",",header=T)
 sessiondat <- read.table("data/raw/GMIMonitoringData.csv",sep=",",header=T)
 
+spatstat.options(checkpolygons=T)
+shape<- as(tmpshape, "owin")
